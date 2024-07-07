52056ac7b06a74576f585881a58db5b0bdb152d0
therealdanmalmx <danmalmx@gmail.com>
2024-07-07 22:12:58 +0100

    Make navbar responsive

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -2,28 +2,102 @@
 	import { PrismicLink } from '@prismicio/svelte';
 	import WordMark from './WordMark.svelte';
 	import ButtonLink from './ButtonLink.svelte';
+	import clsx from 'clsx';
+	import { asLink } from '@prismicio/client';
+	import IconClose from '~icons/ph/x-bold';
+	import IconOpen from '~icons/ph/list-bold';
+	import { page } from '$app/stores';
 
 	/** @type {import("@prismicio/client").Content.SettingsDocument}*/
 	export let settings;
+
+	let isOpen = false;
+	const toggleOpen = () => {
+		isOpen = !isOpen;
+	};
+	const close = () => {
+		isOpen = false;
+	};
+
+	/** @param {import('@prismicio/client').LinkField} link */
+	const isActive = (link) => {
+		const path = asLink(link);
+
+		return path && $page.url.pathname.includes(path);
+	};
 </script>
 
-<footer>
+<header class="p-4 md:p-6">
 	<nav
-		class="flex flex-col items-center justify-between gap-6 border-t border-gray-600 px-8 py-6 md:flex-row"
+		class="mx-auto flex max-w-6xl flex-col justify-between py-2 font-medium text-white md:flex-row md:items-center"
 		aria-label="Main"
 	>
-		<a href="/">
-			<WordMark />
-			<span class="sr-only">{settings.data.site_title}</span>
-		</a>
+		<div class="flex items-center justify-between">
+			<a href="/" on:click={close} class="z-50">
+				<WordMark />
+				<span class="sr-only">{settings.data.site_title}</span>
+			</a>
+			<button
+				aria-expanded={isOpen}
+				type="button"
+				class="block pt-2 text-3xl text-white md:hidden"
+				on:click={toggleOpen}><IconOpen /></button
+			>
+		</div>
 
-		<ul class="flex gap-6">
+		<!-- Mobile Nav -->
+		<div
+			class={clsx(
+				'fixed inset-0 z-40 flex flex-col items-end bg-gray-950 pr-4 pt-6 transition-transform duration-300 ease-in-out md:hidden',
+				isOpen ? 'translate-x-0' : 'translate-x-full'
+			)}
+		>
+			<button
+				aria-expanded={isOpen}
+				type="button"
+				class="block pt-2 text-3xl text-white md:hidden"
+				on:click={toggleOpen}><IconClose /></button
+			>
+			<ul class="grid justify-items-end gap-8">
+				{#each settings.data.navigation as item (item.label)}
+					<li>
+						{#if item.cta_button}
+							<ButtonLink
+								field={item.link}
+								on:click={close}
+								aria-current={isActive(item.link) ? 'page' : undefined}>{item.label}</ButtonLink
+							>
+						{:else}
+							<PrismicLink
+								on:click={close}
+								field={item.link}
+								aria-current={isActive(item.link) ? 'page' : undefined}
+								class="block min-h-11 px-3 text-3xl first:mt-8"
+								>{item.label}
+							</PrismicLink>
+						{/if}
+					</li>
+				{/each}
+			</ul>
+		</div>
+
+		<!-- Desktop Nav -->
+
+		<ul class="hidden gap-6 md:flex">
 			{#each settings.data.navigation as item (item.label)}
 				<li>
 					{#if item.cta_button}
-						<ButtonLink field={item.link}>{item.label}</ButtonLink>
+						<ButtonLink
+							on:click={close}
+							field={item.link}
+							aria-current={isActive(item.link) ? 'page' : undefined}>{item.label}</ButtonLink
+						>
 					{:else}
-						<PrismicLink field={item.link} class="inline-flex min-h-11 items-center"
+						<PrismicLink
+							on:click={close}
+							field={item.link}
+							aria-current={isActive(item.link) ? 'page' : undefined}
+							class="inline-flex min-h-11 items-center"
 							>{item.label}
 						</PrismicLink>
 					{/if}
@@ -31,4 +105,4 @@
 			{/each}
 		</ul>
 	</nav>
-</footer>
+</header>
