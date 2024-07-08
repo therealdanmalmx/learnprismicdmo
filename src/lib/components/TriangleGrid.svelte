0d9108f3625c985f7280278854dd8a83678861fd
therealdanmalmx <danmalmx@gmail.com>
2024-07-08 19:49:13 +0100

    Deploy to Vercel. Add GSAP. Animate hero.

--- src/lib/components/TriangleGrid.svelte
+++ src/lib/components/TriangleGrid.svelte
@@ -1,5 +1,90 @@
 <script>
+	import { onMount } from 'svelte';
+	import gsap from 'gsap';
+
 	const grid = [14, 30];
+
+	onMount(() => {
+		gsap.set('.triangle-grid-item', {
+			opacity: 0,
+			transformOrigin: 'center',
+			color: '#FFFFFF',
+		});
+
+		gsap.set('#triangle-grid', {
+			opacity: 1,
+		});
+
+		const tl = gsap.timeline();
+
+		tl.to('.triangle-grid-item', {
+			keyframes: [
+				{
+					opacity: 0,
+					duration: 0,
+				},
+				{
+					opacity: 0.4,
+					rotate: '+=180',
+					color: '#A78BFA',
+					scale: 3,
+					duration: 0.6,
+					stagger: {
+						amount: 2,
+						grid: grid,
+						from: 'center',
+					},
+				},
+				{
+					opacity: 0.2,
+					rotate: '+=180',
+					color: '#FFFFFF',
+					scale: 1,
+					delay: -2,
+					duration: 0.6,
+					stagger: {
+						amount: 3,
+						grid: grid,
+						from: 'center',
+					},
+				},
+			],
+		});
+
+		// Loop animation
+		tl.to('.triangle-grid-item', {
+			delay: 12,
+			repeat: -1,
+			repeatDelay: 12,
+			keyframes: [
+				{
+					opacity: 0.4,
+					rotate: '+=180',
+					color: '#A78BFA',
+					scale: 3,
+					duration: 0.6,
+					stagger: {
+						amount: 2,
+						grid: grid,
+						from: 'center',
+					},
+				},
+				{
+					opacity: 0.2,
+					rotate: '+=180',
+					color: '#FFFFFF',
+					scale: 1,
+					delay: -2,
+					duration: 0.6,
+					stagger: {
+						amount: 3,
+						grid: grid,
+						from: 'center',
+					},
+				},
+			],
+		});
+	});
 </script>
 
 <svg
